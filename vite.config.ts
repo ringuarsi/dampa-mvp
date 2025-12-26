a2bd65f33ac5374b1b55fbe49e22e9bf06ac04c9
ringuralte <ringuralte@gmail.com>
2025-12-26 13:23:23 +0530

    chore: netlify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import netlify from '@netlify/vite-plugin'
 import netlifyReactRouter from '@netlify/vite-plugin-react-router'
 import { reactRouter } from '@react-router/dev/vite'
 import tailwindcss from '@tailwindcss/vite'
@@ -21,6 +22,7 @@ export default defineConfig({
     }),
     tsconfigPaths(),
     netlifyReactRouter(),
+    netlify(),
   ],
   server: {
     port: 3000,
